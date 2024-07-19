a597422da23e03a20fbfe3fab05e83c2e197c723
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-07-19 16:40:17 +0700

    add worker

---
+++ worker/submission/src/index.js
@@ -0,0 +1,127 @@
+import { z } from "zod"
+import * as Realm from 'realm-web';
+import {
+	toError,
+	addCORS,
+	responseError,
+	responseJSON,
+} from "./utils"
+import { startStudy } from "./studies/startStudy";
+import { fetchStudies } from "./studies/fetchStudies";
+import { handleOptions } from "./handleOptions"
+import { finishStudy } from "./studies/finishStudy";
+import { updateAttentionCheck } from "./studies/updateAttentionCheck";
+
+let App;
+const ObjectId = Realm.BSON.ObjectID;
+
+export default {
+	async fetch(request, env, ctx) {
+		if (request.method === "OPTIONS") {
+			// Handle CORS preflight requests
+			return handleOptions(request);
+		}
+
+		const url = new URL(request.url);
+		App = App || new Realm.App(env.ATLAS_APPID);
+		const method = request.method;
+		const path = url.pathname.replace(/[/]$/, '');
+
+		if (path !== '/api/studies' && path !== '/api/study/finish' && path !== '/api/attention-check') {
+			return responseError(`Unknown '${path}' URL; try '/api/studies' instead.`, 404);
+		}
+
+		// const token = request.headers.get('authorization');
+		const token = "XnabV4Pa2RV6lgyJAj0uAun6X5KM6p0yJceHEm3EJ80757sasEjpP2smYNJaSkcv"
+		if (!token)
+			return responseError(`Missing 'authorization' header; try to add the header 'authorization: ATLAS_APP_API_KEY'.`);
+		try {
+			const credentials = Realm.Credentials.apiKey(token);
+			// Attempt to authenticate
+			var user = await App.logIn(credentials);
+			var client = user.mongoClient('mongodb-atlas');
+		}
+		catch (err) {
+			return responseError('Error with authentication.', 500);
+		}
+
+		try {
+			// console.log("path", path)
+			// GET /api/studies
+			if (path === '/api/studies' && method === 'GET') {
+				console.log("FETCH STUDY")
+				const prolificid = url.searchParams.get('prolificid') || '';
+				const studyid = url.searchParams.get('studyid') || '';
+				const sessionid = url.searchParams.get('sessionid') || '';
+
+				// console.log("prolificid", prolificid, "studyid", studyid, "sessionid", sessionid)
+
+				const { errors, success, data, msg } = await fetchStudies(client, prolificid, studyid, sessionid)
+				// console.log(errors, success, data, msg)
+				return responseJSON({ errors, success, data, msg })
+			}
+
+			// POST /api/studies
+			if (path === '/api/studies' && method === 'POST') {
+				console.log("START STUDY")
+				const { prolificid, studyid, sessionid } = await request.json();
+
+				const { errors, success, data, msg } = await startStudy(client, prolificid, studyid, sessionid)
+
+				return responseJSON({ errors, success, data, msg })
+			}
+
+			// POST /api/studies/finish
+			if (path === '/api/study/finish' && method === 'POST') {
+				console.log("FINISH STUDY API")
+				const {
+					prolificid,
+					studyid,
+					sessionid,
+					actions,
+					screenActions,
+					studySelections,
+					code
+				} = await request.json()
+
+				const { errors, success, data, msg } = await finishStudy(client, prolificid,
+					studyid,
+					sessionid,
+					actions,
+					screenActions,
+					studySelections,
+					code)
+
+				return responseJSON({ errors, success, data, msg })
+			}
+
+			if (path === '/api/attention-check' && method === 'POST') {
+				console.log("ATTENTION CHECK")
+				const {
+					prolificid,
+					studyid,
+					sessionid
+				} = await request.json()
+
+				const { errors, success, data, msg } = await updateAttentionCheck(client, prolificid,
+					studyid,
+					sessionid)
+
+				return responseJSON({ errors, success, data, msg })
+			}
+
+			// unknown method
+			return responseError('Method not allowed.', 405);
+		}
+		catch (err) {
+			console.log(err)
+			return responseError({
+				errors: err,
+				success: false,
+				data: null,
+				msg: "Internal server error",
+			})
+		}
+	},
+
+};
---
+++ worker/submission/src/utils.js
@@ -0,0 +1,57 @@
+export function toJSON(data, status = 200) {
+	let body = JSON.stringify(data, null, 2);
+	let headers = { 'content-type': 'application/json' };
+	return new Response(body, { headers, status });
+}
+export function toError(error, status = 400) {
+	return toJSON({ error }, status);
+}
+export function reply(output) {
+	if (output != null)
+		return toJSON(output, 200);
+	return toError('Error with query', 500);
+}
+
+export function responseWithCORS(data) {
+	const headers = new Headers({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+		'Access-Control-Max-Age': '86400'
+	});
+
+	return new Response(JSON.stringify(data), { headers, status: 200 });
+}
+
+export function responseJSON(data) {
+	const response = Response.json(data, {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+			'Access-Control-Max-Age': '86400'
+		}
+	});
+
+
+	return response
+}
+
+
+export function responseError(data, status = 401) {
+	const headers = new Headers({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+		'Access-Control-Max-Age': '86400'
+	});
+
+	return new Response(JSON.stringify(data), { headers, status });
+}
+
+export function addCORS(response) {
+	response.headers.set('Access-Control-Allow-Origin', '*');
+	response.headers.set('Access-Control-Allow-Methods', 'GET,HEAD,POST,OPTIONS');
+	response.headers.set('Access-Control-Max-Age', '86400');
+
+	return response
+}
