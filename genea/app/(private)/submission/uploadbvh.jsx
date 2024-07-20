1c85e1a043f963e3d917c92157e23b591da41656
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-07-20 17:57:17 +0700

    update upload progress

--- genea/app/(private)/submission/uploadbvh.jsx
+++ genea/app/(private)/submission/uploadbvh.jsx
@@ -21,7 +21,7 @@ export default function UploadBVH({ codes }) {
   const [errorMsg, setErrorMsg] = useState("")
   const [uploading, setUploading] = useState("")
   // const [uploadProgress, setUploadProgress] = useState({})
-  const [progress, setProgress] = useState([])
+  const [progress, setProgress] = useState({})
   const [success, setSuccess] = useState("")
 
   const [email, setEmail] = useState("")
@@ -52,9 +52,10 @@ export default function UploadBVH({ codes }) {
       setMissingList(missing)
       setFiles(acceptedFiles)
       setProgress(
-        Array.from(acceptedFiles).map((fileItem) => {
-          return { fileName: fileItem.name, percent: 0, status: "pending" }
-        })
+        Array.from(acceptedFiles).reduce((progressItems, fileItem) => {
+          progressItems[fileItem.name] = { percent: 0, status: "pending" }
+          return progressItems
+        }, {})
       )
 
       const selectedFiles = Array.from(acceptedFiles).map((file) => ({
@@ -77,13 +78,12 @@ export default function UploadBVH({ codes }) {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
   const updateUploadProgress = useCallback((fileName, percent, status) => {
-    setProgress((prevProgress) =>
-      prevProgress.map((item) => {
-        if (item.fileName === fileName) {
-          return { ...item, percent: percent, status: status }
-        }
-      })
-    )
+    setProgress((prevProgress) => {
+      return {
+        ...prevProgress,
+        [fileName]: { percent: percent, status: status },
+      }
+    })
   }, [])
 
   const uploadFile = async (file, index, userId) => {
