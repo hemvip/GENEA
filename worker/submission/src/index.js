be75c2c8f30d9f6454be93c3e74263e4d06debc8
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-01-22 16:10:33 +0700

    add api for upload video

--- worker/submission/src/index.js
+++ worker/submission/src/index.js
@@ -2,6 +2,9 @@ import { corsHeaders } from "./cors"
 import { handleStartUpload } from "./submission/upload-start"
 import { handleUploadChunk } from "./submission/upload-chunk"
 import { handleCompleteUpload } from "./submission/upload-complete"
+import { handleVideoStartUpload } from "./video/upload-start"
+import { handleVideoUploadChunk } from "./video/upload-chunk"
+import { handleVideoCompleteUpload } from "./video/upload-complete"
 
 export default {
 	async fetch(request, env, ctx) {
@@ -17,12 +20,21 @@ export default {
 		if (request.method === "POST") {
 			const url = new URL(request.url)
 
+			// Npy upload
 			if (url.pathname === "/api/start-upload") {
 				return handleStartUpload(request, env)
 			} else if (url.pathname === "/api/upload-part") {
 				return handleUploadChunk(request, env)
 			} else if (url.pathname === "/api/complete-upload") {
 				return handleCompleteUpload(request, env)
+			}
+			// Video upload
+			else if (url.pathname === "/api/video-start-upload") {
+				return handleVideoStartUpload(request, env)
+			} else if (url.pathname === "/api/video-upload-part") {
+				return handleVideoUploadChunk(request, env)
+			} else if (url.pathname === "/api/video-complete-upload") {
+				return handleVideoCompleteUpload(request, env)
 			}
 			return new Response("API not found", { status: 404, headers: corsHeaders })
 		}
