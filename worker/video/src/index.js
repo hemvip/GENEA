95b8157203898e48d6321a6086a99c1ed4fb1053
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-01-22 16:59:45 +0700

    update worker of video and submission

--- worker/video/src/index.js
+++ worker/video/src/index.js
@@ -1,15 +1,33 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { corsHeaders } from "./cors"
+import { handleVideoStartUpload } from "./video/upload-start"
+import { handleVideoUploadChunk } from "./video/upload-chunk"
+import { handleVideoCompleteUpload } from "./video/upload-complete"
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		if (request.method === "OPTIONS") {
+			// Handle CORS preflight requests
+			return new Response(null, { headers: corsHeaders })
+		}
+
+		if (request.method === "GET") {
+			return new Response(JSON.stringify({ message: "It work" }), { headers: corsHeaders })
+		}
+
+		if (request.method === "POST") {
+			const url = new URL(request.url)
+
+			// Npy upload
+			if (url.pathname === "/api/start-upload") {
+				return handleVideoStartUpload(request, env)
+			} else if (url.pathname === "/api/upload-part") {
+				return handleVideoUploadChunk(request, env)
+			} else if (url.pathname === "/api/complete-upload") {
+				return handleVideoCompleteUpload(request, env)
+			}
+			return new Response("API not found", { status: 404, headers: corsHeaders })
+		}
+
+		return new Response("Not Found", { status: 404 })
 	},
-};
+}
