cc2482c48b103d84323ea86aef2bdfab18e3b7b5
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-01-27 02:54:02 +0700

    update question

--- genea/app/(navbar)/layout.jsx
+++ genea/app/(navbar)/layout.jsx
@@ -1,17 +1,12 @@
-import Sidebar from "@/components/sidebar"
-import { Callout } from "@/nextra"
-import { File } from "@/nextra/sidebar"
-// import { Sidebar } from "@/nextra/sidebar"
 import React from "react"
+import Navbar from "./navbar"
+import Sidebar from "@/components/sidebar"
 
 export default function Layout({ children }) {
   return (
     <div className="mx-auto flex max-w-[90rem]">
       <Sidebar />
-      <nav
-        className="nextra-toc order-last max-xl:hidden w-64 shrink-0 print:hidden"
-        aria-label="table of contents"
-      ></nav>
+      <Navbar />
       <article className="w-full min-w-0 break-words min-h-[calc(100vh-var(--nextra-navbar-height))] text-slate-700 dark:text-slate-200 pb-8 px-6 pt-4 md:px-12">
         <main className="">{children}</main>
       </article>
