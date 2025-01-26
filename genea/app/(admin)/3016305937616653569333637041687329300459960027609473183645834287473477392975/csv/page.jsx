192f3dcb6d491c86f462721fdcf77ab020432ea6
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-01-26 22:24:01 +0700

    complete csv upload and parse

--- genea/app/(admin)/3016305937616653569333637041687329300459960027609473183645834287473477392975/csv/page.jsx
+++ genea/app/(admin)/3016305937616653569333637041687329300459960027609473183645834287473477392975/csv/page.jsx
@@ -7,43 +7,68 @@ import { useEffect, useState } from "react"
 import InputCode from "./inputcode"
 import axios from "axios"
 import { Loading } from "@/components"
+import CSVPreviewer from "./CSVPreviewer"
+import JSON from "@/components/icons/json"
+import CSV from "@/components/icons/csv"
 
 export default function Page() {
-  // const [codes, setCodes] = useState([])
-  const [loading, setLoading] = useState(false)
-  // const [teams, setTeams] = useState([])
+  const [csvList, setCsvList] = useState([])
+  const [loadedCSV, setLoadedCSV] = useState(false)
 
-  // async function fetchInputCodes() {
-  //   setLoading(false)
-  //   const res = await axios.get("/api/inputcode")
-  //   if (res.data.success) {
-  //     setCodes(res.data.codes)
-  //     setLoading(true)
-  //   } else {
-  //     console.error(res.error)
-  //   }
+  // if (loading) {
+  //   return (
+  //     <div className="text-center">
+  //       <Loading />
+  //     </div>
+  //   )
   // }
 
-  // async function fetchTeams() {
-  //   const res = await axios.get("/api/submission")
-  //   if (res.data.success) {
-  //     setTeams(res.data.submissions)
-  //   } else {
-  //     console.error(res.error)
-  //   }
-  // }
+  const handleUpload = async (e) => {
+    e.preventDefault()
+    console.log("csvList", csvList)
 
-  // useEffect(() => {
-  //   fetchTeams()
-  //   // fetchInputCodes()
-  // }, [])
+    // if (files.length <= 0) {
+    //   setErrorMsg("Please upload video")
+    //   return
+    // }
 
-  if (loading) {
-    return (
-      <div className="text-center">
-        <Loading />
-      </div>
-    )
+    // try {
+    //   setUploading("Uploading your videos, please waiting ...")
+
+    //   const uploadPromises = Array.from(files).map((file) => {
+    //     return uploadPromise(file, (fileName, percent) => {
+    //       setProgress((prevProgress) => {
+    //         // console.log("prevProgress", prevProgress)
+    //         return {
+    //           ...prevProgress,
+    //           [fileName]: percent,
+    //         }
+    //       })
+    //     })
+    //   })
+
+    //   const results = await Promise.all(uploadPromises)
+    //   // console.log("results", results)
+    //   const allSuccessful = results.every((result) => result.success)
+    //   if (allSuccessful) {
+    //     const { success, msg, error } = results.at(-1)
+    //     setSuccess(msg)
+    //     console.log("Success", success, "msg", msg, "error", error)
+    //   } else {
+    //     const failedResult = results.filter((result) => !result.success)[0]
+    //     const { success, msg, error } = failedResult
+    //     setErrorMsg(msg)
+    //     console.log("Success", success, "msg", msg, "error", error)
+    //   }
+    // } catch (error) {
+    //   console.log("EXCEPTION ", error)
+    //   setErrorMsg(
+    //     "EXCEPTION: Error with uploading your videos, please contact support"
+    //   )
+    //   console.log("Exception", error)
+    // } finally {
+    //   setUploading("")
+    // }
   }
 
   return (
@@ -53,7 +78,66 @@ export default function Page() {
       </h2>
       <div className="mt-6 mb-32">
         {/* <p className="mt-3 leading-7 first:mt-0">Github information</p> */}
-        <UploadCSV />
+        <form className="mt-6 flex flex-col w-[80%] gap-4">
+          <UploadCSV setCsvList={setCsvList} setLoadedCSV={setLoadedCSV} />
+
+          <div className="flex flex-col py-4 gap-4">
+            {csvList.map(({ data: csvData, filename }, index) => {
+              return (
+                <div
+                  key={index}
+                  data-pagefind-ignore="all"
+                  className="nextra-code relative not-first:mt-6"
+                >
+                  <div className="px-4 text-xs text-gray-700 dark:text-gray-200 bg-gray-100 dark:bg-neutral-900 flex items-center h-10 gap-2 rounded-t-md border border-gray-300 dark:border-neutral-700 contrast-more:border-gray-900 contrast-more:dark:border-gray-50 border-b-0">
+                    <CSV className="w-4 h-4" />
+                    <span className="truncate">{filename}</span>
+                  </div>
+                  <pre
+                    className="group focus-visible:nextra-focus overflow-x-auto subpixel-antialiased text-[.9em] bg-white dark:bg-black py-4 ring-1 ring-inset ring-gray-300 dark:ring-neutral-700 contrast-more:ring-gray-900 contrast-more:dark:ring-gray-50 contrast-more:contrast-150 rounded-b-md not-prose"
+                    tabIndex="0"
+                  >
+                    <div className="group-hover:opacity-100 group-focus:opacity-100 opacity-0 transition focus-within:opacity-100 flex gap-1 absolute right-4 top-14">
+                      <button
+                        className="transition border border-gray-300 dark:border-neutral-700 contrast-more:border-gray-900 contrast-more:dark:border-gray-50 rounded-md p-1.5 md:hidden"
+                        title="Toggle word wrap"
+                        type="button"
+                        data-headlessui-state=""
+                      >
+                        <svg
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          height="16"
+                        >
+                          <path d="M4 19h6v-2H4v2zM20 5H4v2h16V5zm-3 6H4v2h13.25c1.1 0 2 .9 2 2s-.9 2-2 2H15v-2l-3 3l3 3v-2h2c2.21 0 4-1.79 4-4s-1.79-4-4-4z"></path>
+                        </svg>
+                      </button>
+                    </div>
+                    <div className="nextra-code" dir="ltr">
+                      <CSVPreviewer
+                        key={index}
+                        csvData={csvData.slice(1)}
+                        headers={csvData[0]}
+                      />
+                    </div>
+                  </pre>
+                </div>
+              )
+            })}
+          </div>
+          {loadedCSV && (
+            <div className="flex flex-col items-center">
+              <div className=" flex justify-start">
+                <button
+                  className=" flex h-10 items-center gap-2 w-44 betterhover:hover:bg-gray-600 dark:betterhover:hover:bg-gray-300 justify-center rounded-md border border-transparent bg-black px-4 py-2 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-gray-800 dark:bg-white dark:text-black dark:focus:ring-white sm:text-sm  transition-all "
+                  onClick={handleUpload}
+                >
+                  Generate Study
+                </button>
+              </div>
+            </div>
+          )}
+        </form>
       </div>
     </>
   )
--- genea/app/(admin)/3016305937616653569333637041687329300459960027609473183645834287473477392975/csv/uploadcsv.jsx
+++ genea/app/(admin)/3016305937616653569333637041687329300459960027609473183645834287473477392975/csv/uploadcsv.jsx
@@ -10,12 +10,13 @@ import { clsx as cn } from "clsx"
 import BVHFile from "@/components/icons/bvhfile"
 import VideoFile from "@/components/icons/videofile"
 import { ArrowLeftIcon } from "@/nextra/icons"
+import Papa from "papaparse"
 
 // export default function UploadVideos({ codes, teams }) {
-export default function UploadCSV({ teams }) {
+export default function UploadCSV({ setCsvList, loadedCSV, setLoadedCSV }) {
   // const [team, setTeam] = useState(teams[0])
   // const [teamID, setTeamID] = useState(teams[0].userId)
-  const [files, setFiles] = useState([])
+  // const [files, setFiles] = useState([])
   const [previews, setPreviews] = useState([])
   const [errorMsg, setErrorMsg] = useState("")
   const [uploading, setUploading] = useState("")
@@ -23,30 +24,33 @@ export default function UploadCSV({ teams }) {
   const [progress, setProgress] = useState({})
   const [success, setSuccess] = useState("")
 
-  // const [missingList, setMissingList] = useState([])
+  const parseFile = (file) => {
+    return new Promise((resolve, reject) => {
+      Papa.parse(file, {
+        complete: (result) => {
+          // Add the filename to the parsed result
+          const dataWithFilename = {
+            filename: file.name,
+            data: result.data,
+          }
+          resolve(dataWithFilename)
+        },
+        error: (error) => {
+          reject(error)
+        },
+        header: false,
+      })
+    })
+  }
 
-  const onDrop = useCallback(async (acceptedFiles) => {
-    setErrorMsg("")
-    setUploading("")
-    setSuccess("")
-
-    setFiles(acceptedFiles)
-    setProgress({})
-
-    // const selectedFiles = Array.from(acceptedFiles).map((file) => ({
-    //   file,
-    //   url: URL.createObjectURL(file),
-    // }))
-    console.log("selectedFiles", acceptedFiles)
-    setPreviews(acceptedFiles)
-
-    try {
-      // handleUpload()
-      // console.log(response.data.message)
-    } catch (error) {
-      console.error("Error uploading files:", error)
-    }
-    setUploading("")
+  const onDrop = useCallback(async (files) => {
+    const csvListData = await Promise.all(
+      Array.from(files)
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((file) => parseFile(file))
+    )
+    setCsvList(csvListData)
+    setLoadedCSV(true)
   }, [])
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -80,53 +84,6 @@ export default function UploadCSV({ teams }) {
   //     })
   // }
 
-  const handleUpload = async (e) => {
-    e.preventDefault()
-
-    if (files.length <= 0) {
-      setErrorMsg("Please upload video")
-      return
-    }
-
-    try {
-      setUploading("Uploading your videos, please waiting ...")
-
-      const uploadPromises = Array.from(files).map((file) => {
-        return uploadPromise(file, (fileName, percent) => {
-          setProgress((prevProgress) => {
-            // console.log("prevProgress", prevProgress)
-            return {
-              ...prevProgress,
-              [fileName]: percent,
-            }
-          })
-        })
-      })
-
-      const results = await Promise.all(uploadPromises)
-      // console.log("results", results)
-      const allSuccessful = results.every((result) => result.success)
-      if (allSuccessful) {
-        const { success, msg, error } = results.at(-1)
-        setSuccess(msg)
-        console.log("Success", success, "msg", msg, "error", error)
-      } else {
-        const failedResult = results.filter((result) => !result.success)[0]
-        const { success, msg, error } = failedResult
-        setErrorMsg(msg)
-        console.log("Success", success, "msg", msg, "error", error)
-      }
-    } catch (error) {
-      console.log("EXCEPTION ", error)
-      setErrorMsg(
-        "EXCEPTION: Error with uploading your videos, please contact support"
-      )
-      console.log("Exception", error)
-    } finally {
-      setUploading("")
-    }
-  }
-
   if (success) {
     return (
       <div className="w-full p-12 justify-center ">
@@ -142,7 +99,7 @@ export default function UploadCSV({ teams }) {
       <div className="w-full px-12  justify-center ">
         <p className="text-center p-4">Uploading...</p>
         <div className="flex flex-col gap-2">
-          {files.map((file, idx) => {
+          {/* {files.map((file, idx) => {
             return (
               <div
                 className=" mx-32 flex gap-2 items-center border-gray-200 py-2 px-8 shadow"
@@ -171,7 +128,7 @@ export default function UploadCSV({ teams }) {
                 </span>
               </div>
             )
-          })}
+          })} */}
         </div>
         <Callout type="warning" className="mt-0">
           {uploading}
@@ -181,7 +138,7 @@ export default function UploadCSV({ teams }) {
   }
 
   return (
-    <form className="mt-6 flex flex-col w-[80%] px-10 gap-4">
+    <>
       {/* <div className="flex flex-row items-center gap-4">
         <label htmlFor="name" className="w-[20%] flex justify-end">
           Team
@@ -243,13 +200,13 @@ export default function UploadCSV({ teams }) {
       </div> */}
 
       <div className="flex flex-row items-center gap-4">
-        <label htmlFor="upload" className="w-[20%] flex justify-end">
+        {/* <label htmlFor="upload" className="w-[20%] flex justify-end">
           CSV Upload
-        </label>
+        </label> */}
         <div
           {...getRootProps()}
           style={{ border: "2px dashed #666666" }}
-          className="w-[80%] p-4 cursor-pointer rounded-lg min-h-36 flex flex-col items-center justify-center text-center appearance-none border border-[#666666] bg-white text-base text-gray-900 placeholder-gray-500 focus:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:border-[#888888] dark:bg-transparent dark:text-white dark:focus:border-white sm:text-sm"
+          className="w-full p-4 cursor-pointer rounded-lg min-h-36 flex flex-col items-center justify-center text-center appearance-none border border-[#666666] bg-white text-base text-gray-900 placeholder-gray-500 focus:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:border-[#888888] dark:bg-transparent dark:text-white dark:focus:border-white sm:text-sm"
         >
           <input
             id="upload"
@@ -290,17 +247,6 @@ export default function UploadCSV({ teams }) {
           </Callout>
         </div>
       )}
-
-      <div className="flex flex-col items-center">
-        <div className="pl-[20%] flex justify-start">
-          <button
-            className=" flex h-10 items-center gap-2 w-44 betterhover:hover:bg-gray-600 dark:betterhover:hover:bg-gray-300 justify-center rounded-md border border-transparent bg-black px-4 py-2 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-gray-800 dark:bg-white dark:text-black dark:focus:ring-white sm:text-sm  transition-all "
-            onClick={handleUpload}
-          >
-            Generate Study
-          </button>
-        </div>
-      </div>
-    </form>
+    </>
   )
 }
