55c08d692b678832a8f0eab879b0faf72fec52d6
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-02-04 12:07:25 +0700

    remove gap

--- genea/app/(admin)/3016305937616653569333637041687329300459960027609473183645834287473477392975/csv/UploadBox.jsx
+++ genea/app/(admin)/3016305937616653569333637041687329300459960027609473183645834287473477392975/csv/UploadBox.jsx
@@ -158,72 +158,25 @@ export default function UploadBox({ setCsvList, loadedCSV, setLoadedCSV }) {
   // }
 
   return (
-    <>
-      {/* <div className="flex flex-row items-center gap-4">
-        <label htmlFor="name" className="w-[20%] flex justify-end">
-          Team
-        </label>
-        <div className="relative items-center align-middle flex-grow">
-          <Select
-            name="status"
-            onChange={(e) => setTeamID(e.target.value)}
-            className={cn(
-              "block w-full appearance-none py-1.5 px-3 text-sm/6  items-center rounded border border-black",
-              "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25",
-              "text-black"
-            )}
-          >
-            {({ focus, hover }) => (
-              // <select
-              //   className={cn(
-              //     "border",
-              //     focus && "bg-blue-100",
-              //     hover && "shadow",
-              //     "flex-grow min-w-0 appearance-none rounded-md border border-[#666666] bg-white px-4 py-2 text-base text-gray-900 placeholder-gray-500 focus:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:border-[#888888] dark:bg-transparent dark:text-white dark:focus:border-white sm:text-sm"
-              //   )}
-              //   onChange={(e) => setTeamID(e.target.value)}
-              //   aria-label="Project status"
-              // >
-              <>
-                {teams.map((team, index) => (
-                  <option
-                    key={index}
-                    className="text-gray-800 dark:text-gray-100 relative cursor-pointer whitespace-nowrap py-1.5 transition-colors ltr:pl-3 ltr:pr-9 rtl:pr-3 rtl:pl-9"
-                    value={team.userId}
-                  >
-                    {team.teamname}
-                  </option>
-                ))}
-              </>
-              // </select>
-            )}
-          </Select>
-          <ArrowLeftIcon
-            className="pointer-events-none absolute top-2.5 right-2.5 size-5  ltr:rotate-90"
-            aria-hidden="true"
-          />
-        </div>
-      </div> */}
-      <div className="flex flex-row items-center gap-4">
-        <div
-          {...getRootProps()}
-          style={{ border: "2px dashed #666666" }}
-          className="w-full p-4 cursor-pointer rounded-lg min-h-36 flex flex-col items-center justify-center text-center appearance-none border border-[#666666] bg-white text-base text-gray-900 placeholder-gray-500 focus:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:border-[#888888] dark:bg-transparent dark:text-white dark:focus:border-white sm:text-sm"
-        >
-          <input
-            id="upload"
-            {...getInputProps()}
-            type="file"
-            accept=".csv"
-            multiple={true}
-          />
-          {isDragActive ? (
-            <p>Drop csv files here...</p>
-          ) : (
-            <p>Drag and drop csv files here, or click to select csv</p>
-          )}
-        </div>
+    <div className="flex flex-row items-center">
+      <div
+        {...getRootProps()}
+        style={{ border: "2px dashed #666666" }}
+        className="w-full p-4 cursor-pointer rounded-lg min-h-36 flex flex-col items-center justify-center text-center appearance-none border border-[#666666] bg-white text-base text-gray-900 placeholder-gray-500 focus:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:border-[#888888] dark:bg-transparent dark:text-white dark:focus:border-white sm:text-sm"
+      >
+        <input
+          id="upload"
+          {...getInputProps()}
+          type="file"
+          accept=".csv"
+          multiple={true}
+        />
+        {isDragActive ? (
+          <p>Drop csv files here...</p>
+        ) : (
+          <p>Drag and drop csv files here, or click to select csv</p>
+        )}
       </div>
-    </>
+    </div>
   )
 }
